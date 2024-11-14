e467f5841664b25c67db4dc3e10865569ba590bc
Dat Do Duc <doducdat2904@gmail.com>
2024-11-14 10:54:10 +0700

    setup: setup layout for main page

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,5 +1,25 @@
+import { FeedWrapper } from "@/components/feed-wrapper";
+import { StickyWrapper } from "@/components/sticky-wrapper";
+import { Header } from "./header";
+import { UserProgress } from "@/components/user-progress";
+
 const Learn = () => {
-    return <div>Learn</div>;
+    return (
+        <div className="flex flex-row-reverse gap-[48px] px-6">
+            <StickyWrapper>
+                <UserProgress
+                    activeCourses={{ title: "Spanish", imageSrc: "/es.svg" }}
+                    hearts={5}
+                    points={100}
+                    hasActiveSubscription={false}
+                />
+            </StickyWrapper>
+
+            <FeedWrapper>
+                <Header title="Spanish" />
+            </FeedWrapper>
+        </div>
+    );
 };
 
 export default Learn;
