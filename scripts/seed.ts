d1bbbc853ded9cbf91ee5a10029d1c0f1057d4f7
Dat Do Duc <doducdat2904@gmail.com>
2024-11-16 16:52:54 +0700

    setup: updated seed script and created query for learn

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -14,6 +14,11 @@ const main = async () => {
     // Clear all database data
     await db.delete(schema.courses);
     await db.delete(schema.userProgress);
+    await db.delete(schema.units);
+    await db.delete(schema.lessons);
+    await db.delete(schema.challenges);
+    await db.delete(schema.challengeOptions);
+    await db.delete(schema.challengeProgress);
 
     // Insert data
     await db.insert(schema.courses).values([
@@ -44,6 +49,67 @@ const main = async () => {
       },
     ])
 
+    await db.insert(schema.units).values([
+      {
+        id: 1,
+        courseId: 1,
+        title: "Unit 1",
+        description: "Learn the basics of Spanish",
+        order: 1,
+      }
+    ]);
+
+    await db.insert(schema.lessons).values([
+      {
+        id: 1,
+        unitId: 1,
+        order: 1,
+        title: "Nouns"
+      },
+      // {
+      //   id: 2,
+      //   unitId: 1,
+      //   order: 2,
+      //   title: "Verbs"
+      // },
+    ])
+
+    await db.insert(schema.challenges).values([
+      {
+        id: 1,
+        lessonId: 1,
+        type: "SELECT",
+        order: 1,
+        question: 'Which one of these is "the man"?'
+      },
+    ]);
+
+    await db.insert(schema.challengeOptions).values([
+      {
+        id: 1,
+        challengeId: 1,
+        imageSrc: "/man.svg",
+        text: "el hombre",
+        audioSrc: "/es_man.mp3",
+        correct: true,
+      },
+      {
+        id: 2,
+        challengeId: 1,
+        imageSrc: "/woman.svg",
+        text: "la mujer",
+        audioSrc: "/es_woman.mp3",
+        correct: false,
+      },
+      {
+        id: 3,
+        challengeId: 1,
+        imageSrc: "/robot.svg",
+        text: "el robot",
+        audioSrc: "/es_robot.mp3",
+        correct: false,
+      },
+    ])
 
     console.log("Seeding complete");
   } catch (error) {
