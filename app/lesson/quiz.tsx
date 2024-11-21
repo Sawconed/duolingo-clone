83df95024a16af2039c7c40fbfa191a6e75c7841
Dat Do Duc <doducdat2904@gmail.com>
2024-11-21 14:10:17 +0700

    feat: added finish page for completed challenges

--- app/lesson/quiz.tsx
+++ app/lesson/quiz.tsx
@@ -8,6 +8,11 @@ import { Footer } from "./footer";
 import { upsertChallengeProgress } from "@/actions/challenge-progress";
 import { toast } from "sonner";
 import { reduceHearts } from "@/actions/user-progress";
+import { useAudio } from "react-use";
+import Image from "next/image";
+import { ResultCard } from "./result-card";
+import { useRouter } from "next/navigation";
+import Confetti from "react-confetti";
 
 type Props = {
   initialLessonId: number;
@@ -27,9 +32,25 @@ export const Quiz = ({
   initialPercentage,
   userSubscription
 }: Props) => {
+  const router = useRouter();
+
+  const [finishAudio] = useAudio({ src: "/finish.mp3", autoPlay: true })
+
+  const [
+    correctAudio,
+    _c,
+    correctControls
+  ] = useAudio({ src: "/correct.wav" });
+
+  const [
+    incorrectAudio,
+    _i,
+    incorrectControls
+  ] = useAudio({ src: "/incorrect.wav" });
 
   const [pending, startTransition] = useTransition();
 
+  const [lessonId] = useState(initialLessonId);
   const [hearts, setHearts] = useState(initialHearts);
   const [percentage, setPercentage] = useState(initialPercentage);
   const [challenges] = useState(initialLessonChallenges);
@@ -84,6 +105,7 @@ export const Quiz = ({
               return;
             }
 
+            correctControls.play();
             setStatus("correct");
             setPercentage((prev) => prev + 100 / challenges.length);
 
@@ -103,6 +125,7 @@ export const Quiz = ({
               return;
             }
 
+            incorrectControls.play();
             setStatus("wrong");
 
             if (!response?.error) {
@@ -114,12 +137,65 @@ export const Quiz = ({
     }
   }
 
+  if (!challenge) {
+    return (
+      <>
+        {finishAudio}
+        <Confetti
+          width={window.innerWidth}
+          height={window.innerHeight}
+          recycle={false}
+          numberOfPieces={500}
+          tweenDuration={10000}
+        />
+        <div className="flex flex-col gap-y-4 lg:gap-y-8 max-w-lg mx-auto text-center items-center justify-center h-full">
+          <Image
+            src="/finish.svg"
+            alt="Finish"
+            className="hidden lg:block"
+            height={100}
+            width={100}
+          />
+
+          <Image
+            src="/finish.svg"
+            alt="Finish"
+            className="block lg:hidden"
+            height={50}
+            width={50}
+          />
+
+          <h1 className="text-xl lg:text-3xl font-bold text-neutral-700">
+            Great job! <br /> You&apos;ve completed the lesson.
+          </h1>
+          <div className="flex items-center gap-x-4 w-full">
+            <ResultCard
+              variant="points"
+              value={challenges.length * 10}
+            />
+            <ResultCard
+              variant="hearts"
+              value={hearts}
+            />
+          </div>
+        </div >
+        <Footer
+          lessonId={lessonId}
+          status="completed"
+          onCheck={() => router.push("/learn")}
+        />
+      </>
+    )
+  }
+
   const title = challenge.type === "ASSIST"
     ? "Select the correct meaning"
     : challenge.question
 
   return (
     <>
+      {correctAudio}
+      {incorrectAudio}
       <Header
         hearts={hearts}
         percentage={percentage}
